e62cdcd66a414f3697bc441ea194aeabf8cabd03
manimarzani <Manimarzani336@gmail.com>
2025-08-16 19:11:15 +0330

    1392

---
+++ 1392.py
@@ -0,0 +1,9 @@
+x = int (input ("enter the score ....>"))
+if 16<=x<20 :
+    print ("well done")
+elif 10<=x<16 :
+    print ("please study beter ") 
+elif 0<x<10:
+    print ("sorry you faled")
+else :
+     print ("please enter number from 1 to 20")
